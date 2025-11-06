9fc7cee6af93689849d05e6583e9e3427ec138f6
Cherarra <votreemail@votreemail.com>
2025-11-06 15:43:36 +0100

    terminer le tp

--- tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
+++ tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
@@ -1,24 +1,50 @@
 package org.emp.gl.core.launcher;
 
-import org.emp.gl.clients.Horloge ;
+import org.emp.gl.clients.Horloge;
+import org.emp.gl.clients.CompteARebours;
+import org.emp.gl.clients.GHorloge;
+import org.emp.gl.clients.GCompteARebours;
+import org.emp.gl.clients.UI;
+import org.emp.gl.time.service.impl.DummyTimeServiceImpl;
+import org.emp.gl.timer.service.TimerService;
 
-/**
- * Hello world!
- *
- */
+import javax.swing.*;
+
 public class App {
+    public static void main(String[] args) throws InterruptedException {
+        // Démarrer l'interface graphique dans l'EDT Swing
+        SwingUtilities.invokeLater(() -> {
+            try {
+                // CORRECTION : utiliser getSystemLookAndFeelClassName() au lieu de getSystemLookAndFeel()
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-    public static void main(String[] args) {
+            DummyTimeServiceImpl timer = new DummyTimeServiceImpl();
 
-        testDuTimeService();
-    }
+            // Créer le dashboard principal
+            new UI(timer);
 
-    private static void testDuTimeService() {
-        Horloge horloge = new Horloge("Num 1") ;
-    }
+            // Créer quelques composants graphiques par défaut
+            new GHorloge("Horloge Principale", timer);
+            new GCompteARebours("Compte Démo", timer, 15);
 
-    public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+            // Garder les composants console existants
+            new Horloge("Console 1", timer);
+            new Horloge("Console 2", timer);
+            new CompteARebours("Console C1", timer, 5);
+            new CompteARebours("Console C1", timer, 10);
+            new CompteARebours("Console C1", timer, 12);
+            new CompteARebours("Console C1", timer, 14);
+            new CompteARebours("Console C1", timer, 16);
+            new CompteARebours("Console C1", timer, 18);
+            new CompteARebours("Console C1", timer, 20);
+
+        });
+
+        // Laisser le programme tourner plus longtemps pour voir les interfaces graphiques
+        Thread.sleep(30000);
+        System.out.println("Programme terminé ✅");
     }
 }
